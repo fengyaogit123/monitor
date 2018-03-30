c6e01674d31acbdfbc416f8102fac943162a7f8d
fengyao123 <1724238345@qq.com>
2018-03-30 21:35:06 +0800

    'commit'

--- app/controller/v1/home.js
+++ app/controller/v1/home.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const egg_1 = require("egg");
 class HomeController extends egg_1.Controller {
     async index() {
-        this.ctx.body = 'hahah';
+        this.ctx.redirect('http://www.baidu.com');
     }
 }
 exports.default = HomeController;
